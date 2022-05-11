5562121a65b5d440f7ed5f4ed1ddb17165394de2
Mario Zito <mario.zito@treme.io>
2022-05-10 21:29:11 -0300

    + refactor validator methods to validators.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use near_sdk::{AccountId, PanicOnDefault, Promise};
 
 mod definitions;
 mod requests;
+mod validators;
 use definitions::*;
 
 #[near_bindgen]
@@ -70,102 +71,6 @@ impl VerificationContract {
                 Promise::new(validator).transfer(PRIZE_AMOUNT);
             }
         }
-    }
-
-    /* Called by *Validators* */
-
-    // Report the result of the verification. If the verification was not possible,
-    // or the validator will not do it then  the validator must include a
-    // descriptive cause.
-    pub fn report_verification_result(
-        &mut self,
-        validator_id: ValidatorId,
-        subject_id: SubjectId,
-        stated: VerificationState,
-        cause: String,
-    ) {
-        log!(
-            "\nreport_verification_result: Called method ({:?} {:?} {:?})",
-            validator_id,
-            subject_id,
-            stated
-        );
-
-        // check if subject_id exists in verifications
-        assert!(
-            self.verifications
-                .keys_as_vector()
-                .iter()
-                .any(|e| e == subject_id),
-            "report_verification_result: Request not found for subject_id"
-        );
-
-        let mut requested = self.verifications.get(&subject_id).unwrap();
-        let mut changed: Vec<VerificationResult> = Vec::new();
-        for before in requested.results.iter() {
-            if before.validator_id == validator_id {
-                changed.push(VerificationResult {
-                    validator_id: validator_id.to_string(),
-                    result: stated.clone(),
-                    timestamp: "2022-03-31 16:00:00".to_string(),
-                });
-            } else {
-                changed.push(before.clone());
-            }
-        }
-
-        // and update the full request state
-        requested.results = changed.clone();
-        self.verifications.insert(&subject_id, &requested);
-    }
-
-    /// Some NEAR account owner registers itself as a validator.
-    pub fn register_as_validator(&mut self, validator_id: ValidatorId) {
-        log!("{:?}", validator_id);
-        self.validators.push(validator_id)
-    }
-
-    pub fn get_validators_count(&self) -> usize {
-        self.validators.len()
-    }
-
-    /* Private */
-
-    // // When the request is filled, we must select a number of validators at random from the validators pool, and assign them to the request
-    // fn assign_validators(&self, subject_id: SubjectId) -> Vec<ValidatorId> {
-    //     //Vec::new()
-    //     let val1: ValidatorId = self.validators[1].to_string();
-    //     let val2: ValidatorId = self.validators[2].to_string();
-    //     let val3: ValidatorId = self.validators[3].to_string();
-    //     vec![val1, val2, val3]
-    // }
-
-    // Every time we receive a verification result we must evaluate if all verifications have been done, and which is the final result for the request. While the verifications are still in course the request state is Pending.
-    fn evaluate_results(&mut self, results: Vec<VerificationResult>) -> VerificationState {
-        // first check if we have some pending result
-        if results
-            .iter()
-            .any(|e| e.result == VerificationState::Pending)
-        {
-            return VerificationState::Pending;
-        }
-
-        // now check if we have some of it rejected
-        match results
-            .iter()
-            .find(|e| e.result == VerificationState::Rejected)
-        {
-            Some(_) => return VerificationState::Rejected,
-            None => {}
-        }
-
-        // check if we have the min required approvals
-        let approvals: u8 = results.iter().fold(0, |count, _| count + 1);
-        if approvals >= MIN_VALIDATORS {
-            return VerificationState::Approved;
-        }
-
-        VerificationState::Pending
     }
 
     /* Not implemented */
---
+++ src/validators.rs
@@ -0,0 +1,95 @@
+use near_sdk::log;
+use near_sdk::{env, near_bindgen};
+
+use crate::definitions::*;
+
+#[near_bindgen]
+impl VerificationContract {
+    // Report the result of the verification. If the verification was not possible,
+    // or the validator will not do it then  the validator must include a
+    // descriptive cause.
+    /* Called by *Validators* */
+    pub fn report_verification_result(
+        &mut self,
+        validator_id: ValidatorId,
+        subject_id: SubjectId,
+        stated: VerificationState,
+        cause: String,
+    ) {
+        log!(
+            "\nreport_verification_result: Called method ({:?} {:?} {:?})",
+            validator_id,
+            subject_id,
+            stated
+        );
+
+        // check if subject_id exists in verifications
+        assert!(
+            self.verifications
+                .keys_as_vector()
+                .iter()
+                .any(|e| e == subject_id),
+            "report_verification_result: Request not found for subject_id"
+        );
+
+        let mut requested = self.verifications.get(&subject_id).unwrap();
+        let mut changed: Vec<VerificationResult> = Vec::new();
+        for before in requested.results.iter() {
+            if before.validator_id == validator_id {
+                changed.push(VerificationResult {
+                    validator_id: validator_id.to_string(),
+                    result: stated.clone(),
+                    timestamp: "2022-03-31 16:00:00".to_string(),
+                });
+            } else {
+                changed.push(before.clone());
+            }
+        }
+
+        // and update the full request state
+        requested.results = changed.clone();
+        self.verifications.insert(&subject_id, &requested);
+    }
+
+    /// Some NEAR account owner registers itself as a validator.
+    pub fn register_as_validator(&mut self, validator_id: ValidatorId) {
+        log!("{:?}", validator_id);
+        self.validators.push(validator_id)
+    }
+
+    pub fn get_validators_count(&self) -> usize {
+        self.validators.len()
+    }
+
+    /* Private */
+
+    // Every time we receive a verification result we must evaluate if all verifications have been done,
+    // and which is the final result for the request. While the verifications are still in course the
+    // request state is Pending.
+    fn evaluate_results(&mut self, results: Vec<VerificationResult>) -> VerificationState {
+        // first check if we have some pending result
+        if results
+            .iter()
+            .any(|e| e.result == VerificationState::Pending)
+        {
+            return VerificationState::Pending;
+        }
+
+        // now check if we have some of it rejected
+        match results
+            .iter()
+            .find(|e| e.result == VerificationState::Rejected)
+        {
+            Some(_) => return VerificationState::Rejected,
+            None => {}
+        }
+
+        // check if we have the min required approvals
+        let approvals: u8 = results.iter().fold(0, |count, _| count + 1);
+        if approvals >= MIN_VALIDATORS {
+            return VerificationState::Approved;
+        }
+
+        VerificationState::Pending
+    }
+}
